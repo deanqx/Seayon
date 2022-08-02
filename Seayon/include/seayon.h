4382504e81182362a389d29933b282d3ac9c0c1f
deanqx <dean2@kowatsch.de>
2022-08-02 21:37:43 +0200

    seayon.h description rework

--- Seayon/include/seayon.h
+++ Seayon/include/seayon.h
@@ -15,108 +15,136 @@
 #define ReLu(z) (z < 0.0f ? 0.0f : z)
 #define dReLu(a) (a < 0.0f ? 0.0f : 1.0f)
 #define Sigmoid(z) 1.0f / (1.0f + exp(-z))
-#define dSigmoid(a) a * (1.0f - a)
+#define dSigmoid(a) a *(1.0f - a)
 
-/// <summary>
-/// <para>Open source Neural Network library in C++ with lots of easy to use features.</para>
-/// <para>Copyright by deanqx and Sawey</para>
-/// </summary>
+// Open source Neural Network library in C++ with lots of easy to use features. Copyright by deanqx and Sawey
 struct seayon
 {
 	struct Layer
 	{
 		std::vector<float> Neurons;
-		/// <summary>
-		/// <para>Goes from first to second</para>
-		/// <para>Layers[l2].Weights[n2][n1]</para>
-		/// </summary>
+		/**
+		 * Goes from first to second
+		 * @tparam Layers[l2].Weights[n2][n1]
+		 */
 		std::vector<std::vector<float>> Weights;
 		std::vector<float> Biases;
 	};
 	std::vector<Layer> Layers;
 	enum class ActivFunc
 	{
-		SIGMOID, RELU, REMOID
+		SIGMOID,
+		RELU
 	} Activation = ActivFunc::SIGMOID;
 	long Writing = -1;
 
-	/// <summary>Creates simple network where every neuron is connected to each neuron in the next layer.</summary>
-	/// <param name="layerCount">Starts with the input layer. Minimum 2 layers</param>
-	/// <param name="func">Activation function for every neuron.</param>
-	void generate(const std::vector<unsigned>& layerCounts, const ActivFunc a = ActivFunc::SIGMOID, const unsigned seed = 0);
-	/// <summary>saves network to a file</summary>
-	void save(std::ofstream& stream, const bool readFormat = false);
-	/// <summary>Copys network to char array</summary>
-	void save(std::string& stream, const bool readFormat = false);
-	/// <summary>
-	/// <para>Loads savefile</para>
-	/// <para>WARNING: Currupt files will throw an error!</para>
-	/// </summary>
-	void load(std::ifstream& stream, const bool readFormat = false);
-	/// <summary>
-	/// <para>Loads save from string</para>
-	/// <para>WARNING: Currupt files will throw an error!</para>
-	/// </summary>
-	void load(std::string& stream, const bool readFormat = false);
-	void copy(seayon* to);
-	/// <summary>
-	/// <para>Averages weights and biases with anouther Network.</para>
-	/// <param name="with	">If you want to average about multiple Networks.</param>
-	/// </summary>
-	void combine(seayon** with, size_t count);
-	bool equals(seayon* second);
+	/**
+	 * Creates network where every neuron is connected to each neuron in the next layer.
+	 * @param layerCount Starts with the input layer (Minimum 2 layers)
+	 * @param ActivFunc Activation function for every neuron.
+	 */
+	void generate(const std::vector<unsigned> &layerCounts, const ActivFunc a = ActivFunc::SIGMOID, const unsigned seed = 0);
+	// Saves network to a .nn file
+	void save(std::ofstream &stream, const bool readFormat = false);
+	// Copys network to a std::string
+	void save(std::string &file, const bool readFormat = false);
+	/**
+	 * Loads .nn file
+	 * @exception Currupt .nn files will throw an error!
+	 */
+	void load(std::ifstream &stream, const bool readFormat = false);
+	/**
+	 * Loads network from a std::string
+	 * @exception Currupt .nn files will throw an error!
+	 */
+	void load(std::string &file, const bool readFormat = false);
+	void copy(seayon *to);
+	/**
+	 * Combines two networks with the average values.
+	 * @param with List of networks
+	 * @param count How many networks
+	 */
+	void combine(seayon **with, size_t count);
+	bool equals(seayon *second);
 
-	/// <summary>prints values of the network to console</summary>
+	// Prints all values of the network to console.
 	void print();
-	/// <summary>prints values and the cost of the network to console</summary>
-	void print(std::vector<float>& outputs);
-	/// <summary>prints values and the cost of the network to console</summary>
-	void print(std::vector<float>& inputs, std::vector<float>& outputs);
-	/// <summary>prints values and the cost of the network to console</summary>
-	void print(std::vector<std::vector<float>>& inputs, std::vector<std::vector<float>>& outputs);
-	/// <summary>prints values and the cost of the network to console</summary>
-	void print(std::vector<std::vector<std::vector<float>>>& inputs, std::vector<std::vector<std::vector<float>>>& outputs);
-	/// <summary>prints outputs of the network to console</summary>
+	// Prints all values and the cost() of the network to console.
+	void print(std::vector<float> &outputs);
+	// Prints all values and the cost() of the network to console.
+	void print(std::vector<float> &inputs, std::vector<float> &outputs);
+	// Prints all values and the cost() with the accruacy() of the network to console.
+	void print(std::vector<std::vector<float>> &inputs, std::vector<std::vector<float>> &outputs);
+	// Prints all values and the cost() with the accruacy() of the network to console.
+	void print(std::vector<std::vector<std::vector<float>>> &inputs, std::vector<std::vector<std::vector<float>>> &outputs);
+	// Prints only the output layer of the network to console.
 	void printo();
-	/// <summary>prints outputs and the cost of the network to console</summary>
-	void printo(std::vector<float>& outputs);
-	/// <summary>prints outputs and the cost of the network to console</summary>
-	void printo(std::vector<float>& inputs, std::vector<float>& outputs);
-	/// <summary>prints outputs and the cost of the network to console</summary>
-	void printo(std::vector<std::vector<float>>& inputs, std::vector<std::vector<float>>& outputs);
-	/// <summary>prints outputs and the cost of the network to console</summary>
-	void printo(std::vector<std::vector<std::vector<float>>>& inputs, std::vector<std::vector<std::vector<float>>>& outputs);
-	/// <summary>prints outputs and the cost of the network to console</summary>
-	void printheat(std::ofstream& csv);
+	// Prints the output layer and the cost() of the network to console.
+	void printo(std::vector<float> &outputs);
+	// Prints the output layer and the cost() of the network to console.
+	void printo(std::vector<float> &inputs, std::vector<float> &outputs);
+	// Prints the output layer and the cost() with the accruacy() of the network to console.
+	void printo(std::vector<std::vector<float>> &inputs, std::vector<std::vector<float>> &outputs);
+	// Prints the output layer and the cost() with the accruacy() of the network to console.
+	void printo(std::vector<std::vector<std::vector<float>>> &inputs, std::vector<std::vector<std::vector<float>>> &outputs);
 
-	/// <summary>Calculates network output</summary>
+	// Calculates network outputs
 	void pulse();
-	/// <summary>Calculates network output</summary>
-	void pulse(std::vector<float>& inputs);
-	/// <summary>Describes how good the network performs.</summary>
-	/// <param name="outputs">Training "should" outputs</param>
-	float cost(std::vector<float>& outputs);
-	/// <summary>Describes how good the network performs.</summary>
-	/// <param name="inputs">Training inputs, every training in/output needs a fellow.</param>
-	/// <param name="outputs">Training "should" outputs, every training in/output needs a fellow.</param>
-	float cost(std::vector<float>& inputs, std::vector<float>& outputs);
-	/// <summary>Describes how good the network performs.</summary>
-	/// <param name="inputs">Training inputs, every training in/output needs a fellow.</param>
-	/// <param name="outputs">Training "should" outputs, every training in/output needs a fellow.</param>
-	float cost(std::vector<std::vector<float>>& inputs, std::vector<std::vector<float>>& outputs);
-	/// <summary>Describes how good the network performs.</summary>
-	/// <param name="inputs">Training inputs, every training in/output needs a fellow.</param>
-	/// <param name="outputs">Training "should" outputs, every training in/output needs a fellow.</param>
-	float cost(std::vector<std::vector<std::vector<float>>>& inputs, std::vector<std::vector<std::vector<float>>>& outputs);
-	float accruacy(std::vector<std::vector<float>>& inputs, std::vector<std::vector<float>>& outputs);
-	float accruacy(std::vector<std::vector<std::vector<float>>>& inputs, std::vector<std::vector<std::vector<float>>>& outputs);
+	/**
+	 * Calculates network outputs
+	 * @param inputs Set values of the input layer
+	 */
+	void pulse(std::vector<float> &inputs);
+	/**
+	 * Describes how far apart the optimal outputs from the outputs are.
+	 * @param outputs This are the optimal outputs
+	 * @return 0.0 = Best; 1.0 or more = Bad
+	 */
+	float cost(std::vector<float> &outputs);
+	/**
+	 * Describes how far apart the optimal outputs from the outputs are.
+	 * @param inputs Set values of the input layer
+	 * @param outputs This are the optimal outputs
+	 * @return 0.0 = Best; 1.0 or more = Bad
+	 */
+	float cost(std::vector<float> &inputs, std::vector<float> &outputs);
+	/**
+	 * Describes how far apart the optimal outputs from the outputs are.
+	 * @param inputs Set values of the input layer
+	 * @param outputs This are the optimal outputs
+	 * @return 0.0 = Best; 1.0 or more = Bad
+	 */
+	float cost(std::vector<std::vector<float>> &inputs, std::vector<std::vector<float>> &outputs);
+	/**
+	 * Describes how far apart the optimal outputs from the outputs are.
+	 * @param inputs Set values of the input layer
+	 * @param outputs This are the optimal outputs
+	 * @return 0.0 = Best; 1.0 or more = Bad
+	 */
+	float cost(std::vector<std::vector<std::vector<float>>> &inputs, std::vector<std::vector<std::vector<float>>> &outputs);
+	/**
+	 * Describes how often the network would choose the right neurons to fire (neuron value >= 0.5).
+	 * @param inputs Set values of the input layer
+	 * @param outputs This are the optimal outputs
+	 * @return 1.0 = 100%; 0.5f = 50%; 0.0 = 0%
+	 */
+	float accruacy(std::vector<std::vector<float>> &inputs, std::vector<std::vector<float>> &outputs);
+	/**
+	 * Describes how often the network would choose the right neurons to fire (neuron value >= 0.5).
+	 * @param inputs Set values of the input layer
+	 * @param outputs This are the optimal outputs
+	 * @return 1.0 = 100%; 0.5f = 50%; 0.0 = 0%
+	 */
+	float accruacy(std::vector<std::vector<std::vector<float>>> &inputs, std::vector<std::vector<std::vector<float>>> &outputs);
 
-	/// <summary>Backpropagation is a very efficient training algorithm.</summary>
-	/// <param name="inputs">Training inputs, every training in/output needs a fellow.</param>
-	/// <param name="outputs">Training should output, every training in/output needs a fellow.</param>
-	/// <param name="runs">Start with less</param>
-	/// <param name="writelog">Writes detailed information about the progress.</param>
-	/// <param name="learningRate">Too high numbers can over shoot the right weight.</param>
-	/// <param name="momentum">How much the algorithm is allowed to move.</param>
-	void fit(std::vector<std::vector<float>>& inputs, std::vector<std::vector<float>>& outputs, unsigned runs, bool print = true, float learningRate = 0.03f, float momentum = 0.1f);
+	/**
+	 * Backpropagation is an efficient training algorithm which works for many cases.
+	 * @param inputs Set values of the input layer
+	 * @param outputs This are the optimal outputs
+	 * @param runs How many iterations. Tip: Start small with like 5 - 20
+	 * @param print Writes detailed information about the progress to the console.
+	 * @param learningRate Large numbers will take less runs but can "over shoot" the right value. 
+	 * @param momentum Large numbers will take less runs but can "over shoot" the right value. 
+	 */
+	void fit(std::vector<std::vector<float>> &inputs, std::vector<std::vector<float>> &outputs, unsigned runs, bool print = true, float learningRate = 0.03f, float momentum = 0.1f);
 };
